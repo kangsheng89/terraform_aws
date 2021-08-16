43e660a5f2df9a817a44516ef9f77bcfd71f3903
kangsheng89 <ks_wong_89@hotmail.com>
2021-08-16 23:32:07 +0800

    add replica for db

--- rdsdb.tf
+++ rdsdb.tf
@@ -32,7 +32,7 @@ module "rdsdb" {
   backup_window                   = "03:00-06:00"
   enabled_cloudwatch_logs_exports = ["general"]
 
-  backup_retention_period = 0
+  backup_retention_period = 1
   skip_final_snapshot     = true
   deletion_protection     = false
 
@@ -49,6 +49,63 @@ module "rdsdb" {
       value = "utf8mb4"
     }
   ]
+
+  tags = local.common_tags
+  db_instance_tags = {
+    "Sensitive" = "high"
+  }
+  db_option_group_tags = {
+    "Sensitive" = "low"
+  }
+  db_parameter_group_tags = {
+    "Sensitive" = "low"
+  }
+  db_subnet_group_tags = {
+    "Sensitive" = "high"
+  }
+}
+
+
+# Create AWS RDS Database
+module "rdsdb-replica" {
+  source  = "terraform-aws-modules/rds/aws"
+  #version = "2.34.0"
+  version = "3.0.0"
+  
+  identifier = "${var.db_instance_identifier}-replica"
+  replicate_source_db  = module.rdsdb.db_instance_id
+
+  name     = var.db_name  # Initial Database Name
+  username = null
+  password = null
+  port     = 3306
+  availability_zone = "us-east-1b"
+  
+  multi_az               = false
+  subnet_ids             = module.vpc.database_subnets
+  vpc_security_group_ids = [module.db_sg.security_group_id]
+
+  # All available versions: http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_MySQL.html#MySQL.Concepts.VersionMgmt
+  engine               = "mysql"
+  engine_version       = "8.0.20"
+  family               = "mysql8.0" # DB parameter group
+  major_engine_version = "8.0"      # DB option group
+  instance_class       = "db.t3.micro"
+
+  allocated_storage     = 20
+  max_allocated_storage = 100
+  storage_encrypted     = false
+
+  maintenance_window              = "Tue:00:00-Tue:03:00"
+  backup_window                   = "03:00-06:00"
+  enabled_cloudwatch_logs_exports = ["general"]
+
+  backup_retention_period = 0
+  skip_final_snapshot     = true
+  deletion_protection     = false
+
+  # Not allowed to specify a subnet group for replicas in the same region
+  create_db_subnet_group = false
 
   tags = local.common_tags
   db_instance_tags = {
